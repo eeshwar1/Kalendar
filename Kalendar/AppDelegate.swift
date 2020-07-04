b3170618927707ea362e7e83d48f7070a8dd0907
Venky Venkatakrishnan <venkyul.appdev@gmail.com>
2020-07-03 19:29:30 -0500

    Initial Commit

---
+++ Kalendar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Kalendar
+//
+//  Created by Venkateswaran Venkatakrishnan on 7/3/20.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Kalendar/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Kalendar
+//
+//  Created by Venkateswaran Venkatakrishnan on 7/3/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
